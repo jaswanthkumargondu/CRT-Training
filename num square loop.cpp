2a4cd8ef5fbd519e44508f4a4e2afe9942a87cb4
jaswanthkumargondu <127976088+jaswanthkumargondu@users.noreply.github.com>
2024-07-16 14:30:58 +0530

    programs

---
+++ num square loop.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int n,i,j,k,count=0;
+	scanf("%d",&n); 
+	for(i=1;i<=n;i++){
+		for(j=i;j<=n*2-i;j++){
+				printf("%d",n);
+			}printf("\n");
+			
+	}
+	return 0;
+}
